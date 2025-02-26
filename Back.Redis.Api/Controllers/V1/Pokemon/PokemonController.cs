c95f5d577bf5dbd8cdd8c51514df3033188b6770
eriksantos617@gmail.com <eriksantos617@gmail.com>
2025-02-26 13:07:29 -0300

    feat: add controllers

---
+++ Back.Redis.Api/Controllers/V1/Pokemon/PokemonController.cs
@@ -0,0 +1,22 @@
+﻿using Back.Redis.Business.Models.V1.Pokemon;
+using Back.Redis.Business.Services.V1.Pokemon;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back.Redis.Api.Controllers.V1.Pokemon
+{
+    [Route("api/[controller]")]
+    public class PokemonController(IPokemonService pokemonService) : ControllerBase
+    {
+        private readonly IPokemonService _pokemonService = pokemonService;
+
+        [HttpGet("{name}")]
+        [ProducesResponseType(typeof(PokemonResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var result = await _pokemonService.GetByName(name);
+            return Ok(result);
+        }
+    }
+}
